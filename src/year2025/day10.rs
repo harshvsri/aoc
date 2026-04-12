d7375374c3f397aeb12451645dc9ffd6b7ba7c76
Harsh <harshvsri@gmail.com>
2026-04-12 20:39:40 +0530

    Add 2025day10part2

--- src/year2025/day10.rs
+++ src/year2025/day10.rs
@@ -36,11 +36,6 @@ impl Machine {
     // INFO: PART I: This completely ignores that we can take any operation multiple times, intentionally to exploit the pattern found.
     // OBSERVATION: If we press any button even times the effect is cancelled and odd presses result in a toggle
     // So keeping that in mind i should only think of a states that can make me reach to desired state in least number of button presses.
-    //
-    // HACK: PART II: We may need to use combinatorics since the order of the key presse doesnt matters.
-    // And we may also introduce a very hard limit of min and max from the joltages.
-    // Here i will update the new base case which is that it must never exceed the joltages.
-    // In such problems using BFS makes so much sense what we need is the smallest num of the presses.
     pub fn min_operations(&self, op_freq: &mut Vec<usize>, index: usize) -> u64 {
         let mut min = u64::MAX;
 
@@ -69,16 +64,46 @@ impl Machine {
         min = min.min(self.min_operations(op_freq, index + 1));
         return min;
     }
+
+    // INFO: PART II: We may need to use combinatorics since the order of the key presse doesnt matters.
+    // And we may also introduce a very hard limit of min and max from the joltages.
+    // Here i will update the new base case which is that it must never exceed the joltages.
+    // In such problems using BFS makes so much sense what we need is the smallest num of the presses.
+    pub fn min_ops(&self) -> u64 {
+        let mut seen = std::collections::HashSet::from([vec![0; self.joltages.len()]]);
+        let mut queue = std::collections::VecDeque::from([(0, vec![0; self.joltages.len()])]);
+        while let Some((clicks, state)) = queue.pop_front() {
+            if state == self.joltages {
+                return clicks;
+            }
+
+            for op in &self.operations {
+                let mut new_state = state.clone();
+                for &button in op {
+                    new_state[button as usize] += 1;
+                }
+
+                if !new_state
+                    .iter()
+                    .zip(&self.joltages)
+                    .any(|(curr, max)| curr > max)
+                    && seen.insert(new_state.clone())
+                {
+                    queue.push_back((clicks + 1, new_state));
+                }
+            }
+        }
+        unreachable!("There is no valid solution");
+    }
 }
 
+// INFO: For PART II we can think of it like a1*x1 + a2*x2 + a3*x3 + a4*x4 + ... = X
+// where, a1 + a2 + a3 + a4 + ... = k; k > 0 and we need to minimize this sum.
 pub fn solve() {
     let data =
-        std::fs::read_to_string("input.txt").expect("File must be present in the root directory.");
+        std::fs::read_to_string("test.txt").expect("File must be present in the root directory.");
     let machines = data.lines().map(Machine::new).collect::<Vec<_>>();
 
-    let res = machines
-        .iter()
-        .map(|m| m.min_operations(&mut vec![0; m.operations.len()], 0))
-        .sum::<u64>();
+    let res = machines.iter().map(|m| dbg!(m.min_ops())).sum::<u64>();
     println!("Final Res: {res}");
 }
