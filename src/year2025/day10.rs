84b8cac497929c37275d4853fd960cd6679017d3
Harsh <harshvsri@gmail.com>
2026-04-12 15:31:05 +0530

    Add 2025day10part1

--- src/year2025/day10.rs
+++ src/year2025/day10.rs
@@ -1,8 +1,8 @@
 #[derive(Debug)]
-struct Machine {
-    state: Vec<char>,
-    operations: Vec<Vec<u64>>,
-    _joltages: Vec<u64>,
+pub struct Machine {
+    pub state: Vec<char>,
+    pub operations: Vec<Vec<u64>>,
+    pub joltages: Vec<u64>,
 }
 
 impl Machine {
@@ -24,21 +24,30 @@ impl Machine {
             .collect::<Vec<_>>();
 
         let joltage = data[data.len() - 1];
-        let _joltages = parse(&joltage[1..joltage.len() - 1]);
+        let joltages = parse(&joltage[1..joltage.len() - 1]);
 
         Self {
             state,
             operations,
-            _joltages,
+            joltages,
         }
     }
 
-    fn min_operations(&self, groups: &mut Vec<usize>, index: usize) -> u64 {
+    // INFO: PART I: This completely ignores that we can take any operation multiple times, intentionally to exploit the pattern found.
+    // OBSERVATION: If we press any button even times the effect is cancelled and odd presses result in a toggle
+    // So keeping that in mind i should only think of a states that can make me reach to desired state in least number of button presses.
+    //
+    // HACK: PART II: We may need to use combinatorics since the order of the key presse doesnt matters.
+    // And we may also introduce a very hard limit of min and max from the joltages.
+    // Here i will update the new base case which is that it must never exceed the joltages.
+    // In such problems using BFS makes so much sense what we need is the smallest num of the presses.
+    pub fn min_operations(&self, op_freq: &mut Vec<usize>, index: usize) -> u64 {
         let mut min = u64::MAX;
+
         if index == self.operations.len() {
             let mut state = vec!['.'; self.state.len()];
-            for group_idx in groups.iter() {
-                for &op in &self.operations[*group_idx] {
+            for (idx, _) in op_freq.iter().enumerate().filter(|(_, f)| **f != 0) {
+                for &op in &self.operations[idx] {
                     match state[op as usize] {
                         '.' => state[op as usize] = '#',
                         '#' => state[op as usize] = '.',
@@ -46,16 +55,18 @@ impl Machine {
                     }
                 }
             }
+
             if state == self.state {
-                min = min.min(groups.len() as u64);
+                min = min.min(op_freq.iter().sum::<usize>() as u64);
             }
             return min;
         }
 
-        groups.push(index);
-        min = min.min(self.min_operations(groups, index + 1));
-        groups.pop();
-        min = min.min(self.min_operations(groups, index + 1));
+        op_freq[index] = 1;
+        min = min.min(self.min_operations(op_freq, index + 1));
+        op_freq[index] = 0;
+
+        min = min.min(self.min_operations(op_freq, index + 1));
         return min;
     }
 }
@@ -67,7 +78,7 @@ pub fn solve() {
 
     let res = machines
         .iter()
-        .map(|m| m.min_operations(&mut Vec::with_capacity(m.operations.len()), 0))
+        .map(|m| m.min_operations(&mut vec![0; m.operations.len()], 0))
         .sum::<u64>();
     println!("Final Res: {res}");
 }
