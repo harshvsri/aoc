61fdf375b703e3b13dc5d6960cc4ef5d69e0d0e6
Harsh <harshvsri@gmail.com>
2026-05-03 07:39:11 +0530

    Update 2024day24part1

--- src/year2024/day24.rs
+++ src/year2024/day24.rs
@@ -1,40 +1,36 @@
 use std::collections::HashMap;
 
-const CONTENT: &'static str = include_str!("../../input.txt");
-
-struct Operation {
-    a: &'static str,
-    b: &'static str,
-    op: &'static str,
-    res: &'static str,
+struct Operation<'a> {
+    a: &'a str,
+    b: &'a str,
+    op: &'a str,
+    res: &'a str,
 }
 
-impl Operation {
-    fn eval(&self, map: &mut HashMap<&'static str, u8>) -> bool {
-        if let Some(_) = map.get(self.res) {
-            return true;
+impl<'a> Operation<'a> {
+    fn eval(&self, map: &mut HashMap<&'a str, u8>) -> bool {
+        if let (Some(a), Some(b)) = (map.get(self.a), map.get(self.b)) {
+            map.insert(
+                self.res,
+                match self.op {
+                    "AND" => a & b,
+                    "OR" => a | b,
+                    "XOR" => a ^ b,
+                    _ => panic!("Invalid operator found"),
+                },
+            );
+            true
+        } else {
+            false
         }
-        let (a, b) = match (map.get(self.a), map.get(self.b)) {
-            (Some(a), Some(b)) => (a, b),
-            _ => return false,
-        };
-
-        map.insert(
-            self.res,
-            match self.op {
-                "AND" => a & b,
-                "OR" => a | b,
-                "XOR" => a ^ b,
-                _ => panic!("Invalid operator found"),
-            },
-        );
-
-        return true;
     }
 }
 
-pub fn foo() {
-    let (operators, operations) = CONTENT
+pub fn solve() {
+    let content = std::fs::read_to_string("input.txt")
+        .expect("input.txt must be present in the root of the directory.");
+
+    let (operators, operations) = content
         .split_once("\n\n")
         .expect("Must constain a valid delimeter");
 
@@ -51,39 +47,48 @@ pub fn foo() {
     let mut operations = operations
         .lines()
         .map(|line| {
-            let (l, res) = line
+            let (values, res) = line
                 .split_once(" -> ")
                 .expect("Must constain a valid delimeter");
 
-            let [a, op, b]: [&str; 3] = l
-                .split_whitespace()
-                .collect::<Vec<_>>()
-                .try_into()
-                .expect("Must contain exactly 3 symbols.");
+            let mut values = values.split_whitespace();
+            let (a, op, b) = (
+                values.next().unwrap(),
+                values.next().unwrap(),
+                values.next().unwrap(),
+            );
             Operation { a, b, op, res }
         })
         .collect::<Vec<_>>();
 
     loop {
-        let pending_ops = operations.len();
         operations.retain(|op| !op.eval(&mut map));
         if operations.is_empty() {
             break;
         }
-        if operations.len() == pending_ops {
-            println!("DEADLOCK!!!");
-            break;
-        }
     }
 
-    let mut res = map
-        .iter()
-        .filter(|op| op.0.starts_with("z"))
-        .collect::<Vec<_>>();
-    res.sort();
-    res.reverse();
-    println!("{:?}", res);
+    // let mut res = map
+    //     .iter()
+    //     .filter(|op| op.0.starts_with("z"))
+    //     .collect::<Vec<_>>();
+    //
+    // res.sort();
+    // res.reverse();
+    //
+    // let decimal = res
+    //     .iter()
+    //     .fold(0u64, |acc, (_, val)| (acc << 1) | **val as u64);
 
-    let decimal = res.iter().fold(0u64, |acc, x| (acc << 1) | *x.1 as u64);
+    let mut decimal = 0u64;
+    for i in 0.. {
+        // Well this make an allocation per key.
+        let key = format!("z{:02}", i);
+        if let Some(&val) = map.get(key.as_str()) {
+            decimal |= (val as u64) << i;
+        } else {
+            break; // Stop when we hit a 'z' bit that doesn't exist
+        }
+    }
     println!("{}", decimal);
 }
