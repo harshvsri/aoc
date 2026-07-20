1d3561dfcb9545db50b26a6327fd24f3fe8f26ee
Harsh <harshvsri@gmail.com>
2026-07-21 02:18:06 +0530

    Add 2022day8part2

--- src/year2022/day8.rs
+++ src/year2022/day8.rs
@@ -2,34 +2,35 @@ pub fn solve() {
     let data =
         std::fs::read_to_string("input.txt").expect("File must be present in the root directory.");
 
-    let mut count = 0;
+    let mut max_view_value = 1;
     let map = parse_map(&data);
     let dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)];
     for x in 0..map.len() {
         for y in 0..map[0].len() {
             let height = map[x][y];
 
+            let mut view_value = 1;
             for &(dx, dy) in &dirs {
                 let (mut nx, mut ny) = (x as isize + dx, y as isize + dy);
 
-                let mut visible = true;
+                let mut tree_count = 0;
                 while nx >= 0 && nx < map.len() as isize && ny >= 0 && ny < map[0].len() as isize {
+                    tree_count += 1;
                     if map[nx as usize][ny as usize] >= height {
-                        visible = false;
                         break;
                     }
                     nx += dx;
                     ny += dy;
                 }
-                if visible {
-                    count += 1;
+                view_value *= tree_count;
+                if view_value == 0 {
                     break;
                 }
-                // Else we need to continue looking into other directions.
             }
+            max_view_value = max_view_value.max(view_value);
         }
     }
-    println!("Visible trees: {count}");
+    println!("Max View: {max_view_value}");
 }
 
 fn parse_map(s: &str) -> Vec<Vec<u8>> {
