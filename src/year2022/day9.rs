5f90a2eab531a0df154e0f4f7b65e15659ba4b2e
Harsh <harshvsri@gmail.com>
2026-07-20 21:39:22 +0530

    Add 2022day9part2

--- src/year2022/day9.rs
+++ src/year2022/day9.rs
@@ -1,47 +1,54 @@
-pub fn solve() {
-    let data =
-        std::fs::read_to_string("input.txt").expect("File must be present in the root directory.");
-
-    let moves = Game::parse_moves(&data);
-    Game::default().play(&moves);
-}
-
-#[derive(Clone, Debug, Default, Eq, Hash, PartialEq)]
+#[derive(Clone, Copy, Eq, Hash, PartialEq)]
 struct Pos {
     x: isize,
     y: isize,
 }
 
-#[derive(Default)]
 pub struct Game {
-    head: Pos,
-    tail: Pos,
+    moves: Vec<(u8, u8)>,
+    rope: [Pos; 10],
 }
 
 impl Game {
-    pub fn parse_moves(s: &str) -> Vec<(u8, u8)> {
-        s.lines()
-            .map(|line| {
-                let (dir, count) = line
-                    .split_once(' ')
-                    .expect("Must contain a valid delemeter");
-                (
-                    dir.as_bytes()[0],
-                    count.parse::<u8>().expect("Must be a valid number"),
-                )
-            })
-            .collect::<Vec<_>>()
+    pub fn init(moves_str: &str) -> Self {
+        fn parse_moves(moves_str: &str) -> Vec<(u8, u8)> {
+            moves_str
+                .lines()
+                .map(|line| {
+                    let (dir, count) = line
+                        .split_once(' ')
+                        .expect("Must contain a valid delemeter");
+                    (
+                        dir.as_bytes()[0],
+                        count.parse::<u8>().expect("Must be a valid number"),
+                    )
+                })
+                .collect::<Vec<_>>()
+        }
+
+        Self {
+            moves: parse_moves(moves_str),
+            rope: [Pos { x: 0, y: 0 }; 10],
+        }
     }
 
-    fn update_tail(&mut self) {
-        if self.tail.x.abs_diff(self.head.x) <= 1 && self.tail.y.abs_diff(self.head.y) <= 1 {
+    fn update_rope(&mut self, index: usize) {
+        if index == self.rope.len() {
             return;
         }
-        self.tail.x += (self.head.x - self.tail.x).signum();
-        self.tail.y += (self.head.y - self.tail.y).signum();
+
+        if self.rope[index].x.abs_diff(self.rope[index - 1].x) <= 1
+            && self.rope[index].y.abs_diff(self.rope[index - 1].y) <= 1
+        {
+            return;
+        }
+        self.rope[index].x += (self.rope[index - 1].x - self.rope[index].x).signum();
+        self.rope[index].y += (self.rope[index - 1].y - self.rope[index].y).signum();
+
+        self.update_rope(index + 1);
     }
 
-    pub fn play(&mut self, moves: &[(u8, u8)]) {
+    pub fn play(&mut self) {
         fn dir_to_coord(dir: u8) -> (isize, isize) {
             match dir {
                 b'U' => (0, 1),
@@ -52,17 +59,24 @@ impl Game {
             }
         }
 
-        let mut visited = std::collections::HashSet::from([self.tail.clone()]);
-        for &(dir, count) in moves {
+        let mut visited = std::collections::HashSet::from([self.rope[self.rope.len() - 1]]);
+        for (dir, count) in std::mem::take(&mut self.moves) {
             let (dx, dy) = dir_to_coord(dir);
             for _ in 0..count {
-                self.head.x += dx;
-                self.head.y += dy;
+                self.rope[0].x += dx;
+                self.rope[0].y += dy;
 
-                self.update_tail();
-                visited.insert(self.tail.clone());
+                self.update_rope(1);
+                visited.insert(self.rope[self.rope.len() - 1]);
             }
         }
         println!("Path len: {}", visited.len());
     }
+}
+
+pub fn solve() {
+    let data =
+        std::fs::read_to_string("input.txt").expect("File must be present in the root directory.");
+
+    Game::init(&data).play();
 }
